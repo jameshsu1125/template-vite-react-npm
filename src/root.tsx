e8fd0acd790da22dd67f4e65521c345210fd0d89
James Hsu <jameshsu1125@gmail.com>
2025-08-06 15:09:42 +0800

    刪除不必要的 index.html 檔案，簡化 root.tsx 中的渲染邏輯

--- src/root.tsx
+++ src/root.tsx
@@ -1,6 +1,4 @@
-import ReactDOM from "react-dom/client";
-import App from ".";
+import ReactDOM from 'react-dom/client';
+import App from '.';
 
-if (document.getElementById("app")?.children.length === 0) {
-  ReactDOM.createRoot(document.getElementById("app")!).render(<App />);
-}
+ReactDOM.createRoot(document.getElementById('app')!).render(<App />);
